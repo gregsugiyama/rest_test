d4dd0ca9e3f1901617dbc5b3a269ba25fa5315f4
gregsugiyama <gjsugiyama@gmail.com>
2021-11-30 00:45:18 -0800

    adds request worker

---
+++ src/rest_test.clj
@@ -0,0 +1,118 @@
+(ns rest-test
+  (:require
+   [taoensso.timbre        :as timbre]
+   [clj-http.client        :as http]
+   [clojure.core.async     :as a]
+   [clojure.edn            :as edn]
+   [clojure.data.json      :as json]
+   [camel-snake-kebab.core :as csk]))
+
+;; ** Helper Functions
+
+(defn remove-vals
+  "Remove keys whose value matches a given `pred`icate."
+  [pred m]
+  (reduce (fn [m' [k v]]
+            (if (pred v)
+              m'
+              (assoc m' k v)))
+          {}
+          m))
+
+(defn empty-or-nil?
+  "True for values that are an empty collection, the empty string,
+  the empty sequence, or `nil`. False for all other values."
+  [v]
+  (or (and (seqable? v)
+           (empty? v))
+      (nil? v)))
+
+(def remove-empty-or-nil-values
+  (partial remove-vals empty-or-nil?))
+
+(defn parse-amount
+  "Converts to a floating point number rounded to the nearest 100th"
+  [n]
+  (java.lang.Float/parseFloat (format "%.2f" n)))
+
+(defn make-url
+  "Returns a url for 'n'.json"
+  [n]
+  (str "https://resttest.bench.co/transactions/" n ".json"))
+
+(defn make-request
+  "Creates a http 'get' request for the provided 'n' page of transaction records.
+   Throws an exception for any non 200 OK status."
+  [n]
+  (let [url (make-url n)]
+    (timbre/info "Sending Request To: " url)
+    (try
+      (http/get url)
+      (catch Exception e (timbre/error "Bad Request" e)))))
+
+(defn get-tally
+  "Makes a 'get' request to the Bench API for a specific page of tallies. 
+   When 200 OK, returns the response body in the form of a clojure map."
+  [page-number]
+  (let [initial-request (make-request page-number)]
+    (when (= 200 (:status initial-request))
+      (let [response-body (json/read-str (:body initial-request)
+                                         :key-fn csk/->kebab-case-keyword)]
+        response-body))))
+
+(defn make-tally
+  "Sorts tansactions by date & sums the transaction costs. 
+   Returns a map of {date transaction-total}"
+  [state]
+  (let [transactions     (->>
+                          (remove-empty-or-nil-values state)
+                          (map (fn [[_ v]]
+                                 (:transactions v)))
+                          (flatten)
+                          (group-by #(:date %)))]
+    (into {} (map (fn [[k v]]
+                    (let [sum (reduce (fn [acc m]
+                                        (let [amount (edn/read-string (:amount m))]
+                                          (+ acc amount))) 0 v)]
+                      {k (parse-amount sum)})) transactions))))
+
+;; -------------------------------------------------------------------------------------- ;;
+
+;; ** In-memory data store
+
+(defonce *state (atom {}))
+
+;; -------------------------------------------------------------------------------------- ;;
+
+;; ** Async Request Worker 
+
+(defn request-worker
+  "Creates an async job que for processing http-requests to the Bench API.
+   Spins up a non-blocking process (channel) to run concurrently on a seperate thread. Once a http-response
+   has been received, it is pulled off the request-channel & stored in memory. The channel
+   will close after all requests have been processed."
+  []
+
+;; Currently, I'm incrementing the page request after a succesfull response. 
+;; Once the que receives a 404, it will stop sending requests & tally the stored responses.
+
+;; One potential drawback is in the url generation logic. What if pages 3 & 6 exist, but not 5? Unlikley, but ultimatly possible. 
+;; That could result in missing data.
+
+  (let [req-chan (a/chan)]
+    (a/go (while true
+            (let [response (a/<! req-chan)]
+              (swap! *state assoc (:page response) response))))
+    (loop [page 1]
+      (let [request (get-tally page)]
+        (if request
+          (do (a/>!! req-chan request)
+              (recur (inc page)))
+          (do (make-tally @*state)
+              (a/close! req-chan)))))))
+
+
+
+(comment
+  (make-tally @*state)
+  (request-worker))
